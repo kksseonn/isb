f5ac53300e8d4de51450b64c16d8d8986e8ebd3d
kksseonn <kote13241@gmail.com>
2024-04-01 21:21:47 +0400

    fixed

--- lab_2/NIST_test.py
+++ lab_2/NIST_test.py
@@ -1,24 +1,21 @@
 import json
 from collections import Counter
 import logging
+from math import erf, sqrt
 
 logging.basicConfig(level=logging.INFO)
 
+
 def frequency_bit_test(sequence):
-    sequence = [int(bit) for bit in sequence]
-    
+    sequence = [1 if int(bit) == 1 else -1 for bit in sequence]
     counts = Counter(sequence)
+    n_negatives = counts[-1]
+    n_positives = counts[1]
+    total_length = n_negatives + n_positives
+    value = (n_positives - n_negatives) / sqrt(total_length)
+    p_value = 0.5 * (1 + erf(abs(value) / sqrt(2)))
     
-    n_zeros = counts[0]
-    n_ones = counts[1]
-    total_bits = n_zeros + n_ones
-    
-    expected_ones = total_bits / 2
-    expected_zeros = total_bits / 2
-    
-    chi_square = ((n_zeros - expected_zeros) ** 2) / expected_zeros + ((n_ones - expected_ones) ** 2) / expected_ones
-    
-    return chi_square
+    return p_value
 
 def write_to_file(file_path: str, data: str) -> None:
     """
@@ -37,8 +34,8 @@ if __name__ == "__main__":
     with open("lab_2/options.json", "r") as options_file:
         options = json.load(options_file)
     
-    c_plus_plus_chi_square = frequency_bit_test(options['c++_sequence'])
-    write_to_file(options['file_name'], f"C++ sequence: {c_plus_plus_chi_square:.16f}")
+    c_plus_plus_bit_test = frequency_bit_test(options['c++_sequence'])
+    write_to_file(options['file_name'], f"C++ sequence: {c_plus_plus_bit_test:.16f}")
     
-    java_chi_square = frequency_bit_test(options['java_sequence'])
-    write_to_file(options['file_name'], f"Java sequence: {java_chi_square:.16f}")
+    java_bit_test = frequency_bit_test(options['java_sequence'])
+    write_to_file(options['file_name'], f"Java sequence: {java_bit_test:.16f}")
