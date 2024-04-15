f8587d7f2e5cecb68105683b68fbd56077b71c77
kksseonn <kote13241@gmail.com>
2024-04-15 16:15:09 +0400

    fixed

--- lab_3/cryptography_utils.py
+++ lab_3/cryptography_utils.py
@@ -1,20 +1,17 @@
 import os
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa, padding as asymmetric_padding
-from cryptography.hazmat.primitives import padding
-from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
+from cryptography.hazmat.primitives import padding, hashes
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
+from file_utils import read_file, write_file
 
 def generate_keys(options):
-    symmetric_key_path = options['symmetric_key']
     public_key_path = options['public_key']
     private_key_path = options['private_key']
     
     symmetric_key = os.urandom(16)
-    with open(symmetric_key_path, 'wb') as f:
-        f.write(symmetric_key)
-    
     private_key = rsa.generate_private_key(
         public_exponent=65537,
         key_size=2048
@@ -32,25 +29,7 @@ def generate_keys(options):
             format=serialization.PrivateFormat.TraditionalOpenSSL,
             encryption_algorithm=serialization.NoEncryption()
         ))
-    
-    print("Keys generated successfully.")
 
-def encrypt_data(options):
-    input_file_path = options['input_file']
-    public_key_path = options['public_key']
-    encrypted_key_path = options['encrypted_key']
-    output_file_path = options['output_file']
-    
-    with open(input_file_path, 'rb') as f:
-        plaintext = f.read()
-    
-    symmetric_key = os.urandom(16)
-    
-    with open(public_key_path, 'rb') as f:
-        public_key = serialization.load_pem_public_key(
-            f.read(),
-            backend=default_backend()
-        )
     encrypted_key = public_key.encrypt(
         symmetric_key,
         asymmetric_padding.OAEP(
@@ -59,11 +38,24 @@ def encrypt_data(options):
             label=None
         )
     )
-    with open(encrypted_key_path, 'wb') as f:
-        f.write(encrypted_key)
+    write_file(options['encrypted_key'], encrypted_key)
     
+    print("Keys generated successfully.")
+
+def encrypt_data(options):
+    output_file_path = options['output_file']
+
+    plaintext = read_file(options['input_file'])
+    encrypted_key = read_file(options['encrypted_key'])
+    private_bytes = read_file(options['private_key'])
+
+    d_private_key = load_pem_private_key(private_bytes,password=None,)
+    
+    symmetric_key = d_private_key.decrypt(encrypted_key, asymmetric_padding.OAEP(mgf=asymmetric_padding.MGF1(algorithm=hashes.SHA256()),
+                                                            algorithm=hashes.SHA256(), label=None))
+
     iv = os.urandom(16)
-    cipher = Cipher(algorithms.AES(symmetric_key), modes.CBC(iv))
+    cipher = Cipher(algorithms.SM4(symmetric_key), modes.CBC(iv))
     encryptor = cipher.encryptor()
     padder = padding.PKCS7(128).padder()
     padded_plaintext = padder.update(plaintext) + padder.finalize()
@@ -78,12 +70,9 @@ def encrypt_data(options):
 def decrypt_data(options):
     input_file_path = options['input_file']
     private_key_path = options['private_key']
-    encrypted_key_path = options['encrypted_key']
-    output_file_path = options['output_file']
     
-    with open(encrypted_key_path, 'rb') as f:
-        encrypted_key = f.read()
-    
+    encrypted_key = read_file(options['encrypted_key'])
+
     with open(private_key_path, 'rb') as f:
         private_key = serialization.load_pem_private_key(
             f.read(),
@@ -103,13 +92,12 @@ def decrypt_data(options):
         iv = f.read(16)
         ciphertext = f.read()
     
-    cipher = Cipher(algorithms.AES(symmetric_key), modes.CBC(iv))
+    cipher = Cipher(algorithms.SM4(symmetric_key), modes.CBC(iv))
     decryptor = cipher.decryptor()
     unpadder = padding.PKCS7(128).unpadder()
     plaintext = decryptor.update(ciphertext) + decryptor.finalize()
     plaintext = unpadder.update(plaintext) + unpadder.finalize()
     
-    with open(output_file_path, 'wb') as f:
-        f.write(plaintext)
+    write_file(options['output_file'], plaintext)
     
     print("Data decrypted successfully.")
